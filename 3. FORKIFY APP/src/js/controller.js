6abff5cf06ed91c44ce72abf82888a86ad90b62f
Shimberly <kimberly.munoz.cep@gmail.com>
2026-08-20 10:24:22 -0600

    LoadRecipes with MVC Arquitecture

--- 3. FORKIFY APP/src/js/controller.js
+++ 3. FORKIFY APP/src/js/controller.js
@@ -1,3 +1,10 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+//console.log(icons); //with this Parcel can take the correct route when building
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +18,28 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  // the await will stop the code until the fetch is loaded, thats why we put in a aync function, async makes the function load in background
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return; //Guard clause
+    recipeView.renderSpinner();
+
+    //1. Loading recipe
+    await model.loadRecipe(id);
+
+    //2. Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+//window.addEventListener('hashchange', controlRecipes);
+//window.addEventListener('load', controlRecipes);
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes),
+);
