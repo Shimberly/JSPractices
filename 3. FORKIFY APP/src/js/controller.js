51c23603d91853551fcb2b78feae52949fac6d9b
Shimberly <kimberly.munoz.cep@gmail.com>
2026-08-26 17:16:00 +0200

    Adding search recipe function

--- 3. FORKIFY APP/src/js/controller.js
+++ 3. FORKIFY APP/src/js/controller.js
@@ -1,29 +1,25 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+//import 'core-js/stable';
+
 //console.log(icons); //with this Parcel can take the correct route when building
 
-const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// https://forkify-api.herokuapp.com/v2
-
 ///////////////////////////////////////
+if (module.hot) {
+  //when saving it would no reload the page if the change it affects just the console
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   // the await will stop the code until the fetch is loaded, thats why we put in a aync function, async makes the function load in background
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
+    console.log(`Recipe id: ${id}`);
 
     if (!id) return; //Guard clause
     recipeView.renderSpinner();
@@ -34,12 +30,48 @@ const controlRecipes = async function () {
     //2. Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
-    alert(error);
+    //alert(error);
+    recipeView.renderError();
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // 1. Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. Load search results
+    await model.loadSearchResults(query);
+
+    // 3. Render results
+    //console.log(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4. Render the initial pagination BUTTONS
+    paginationView.render(model.state.search);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  console.log(`goToPage: ${goToPage}`);
+  // 3. Render NEW results
+  //console.log(model.state.search.results);
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 4. Render NEW pagination BUTTONS
+  paginationView.render(model.state.search);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
+
 //window.addEventListener('hashchange', controlRecipes);
 //window.addEventListener('load', controlRecipes);
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes),
-);
--- 3. FORKIFY APP/src/js/model.js
+++ 3. FORKIFY APP/src/js/model.js
@@ -1,17 +1,22 @@
+import { async } from 'regenerator-runtime';
+import { API_URL, RES_PER_PAGE } from './config';
+import { getJSON } from './helpers';
+import { RES_PER_PAGE } from './config';
+
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
-    );
-    const data = await res.json();
+    const data = await getJSON(`${API_URL}${id}`);
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    console.log(res, data);
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -26,6 +31,37 @@ export const loadRecipe = async function (id) {
     console.log(recipe);
     // this function will not return anything, it just update the state object
   } catch (error) {
-    alert(error);
+    console.log(`${error}`);
+    throw error;
   }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(recipe => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        image: recipe.image_url,
+      };
+    });
+    //console.log(state.search.results);
+  } catch (error) {
+    console.log(`${error}`);
+    throw error;
+  }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage; //0;
+  const end = page * state.search.resultsPerPage; //9;
+
+  return state.search.results.slice(start, end);
 };
